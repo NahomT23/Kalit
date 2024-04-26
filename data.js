29ce5226742266efc20f5b96d925c28ec4ca43ea
NahomT23 <167655953+NahomT23@users.noreply.github.com>
2024-04-26 19:01:55 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,268 @@
+let shopItemData = [
+    {
+        id:"aa",
+        name:"Neutra Chronograph Brown Leather Watch",
+        price: 9000,
+        desc:"FS 5453",
+        img:"img/a.jpg",
+        popupImages: ["img/a2.jpg", "img/a3.jpg", "img/a4.jpg", "img/a.jpg"],
+        spec:` This 44mm Neutra features a blue satin dial with stick indices, chronograph movement and a brown leather strap.
+        Sku: FS5453 <br>
+        Case Size: 44MM <br>
+        Movement: Quartz Chronograph<br>
+        Platform: Neutra Chrono<br>
+        Strap Material: Leather<br>
+        Strap Color: Brown<br>
+        Case Water Resistance: 5 ATM<br>
+        Case Material: Stainless Steel<br>
+        Case Color: Silver<br>
+        Dial Color: Blue<br>
+        Strap Fashion Color: Brown<br>
+        Interchangeable Compatibility: 22MM<br>
+        Strap Width: 22MM<br>
+        Closure: Single Prong Strap Buckle<br>
+        Strap Inner Circumference: 200+/- 5MM<br>
+        Battery Type: SR621SW<br>`
+    },
+    {
+        id:"bb",
+        name:"Citizen eco drive AT2444-54E",
+        price: 18000 ,
+        desc:"AT2444-54E ",
+        img:"img/b.jpg",
+        popupImages: ["img/b4.jpg", "img/b2.jpg", "img/b.jpg", "img/b3.jpg"],
+        spec: `MODEL : AT2146-59E <br>
+        MOVEMENT TECHNOLOGY : H504 MODEL <br>
+        Eco-Drive
+        Powered by any light source, continuously and sustainably, eliminating the need to replace watch batteries.
+        BAND LENGTH (MM): 203.2 <br>
+        CASE WIDTH (MM) : 42 <br>
+        LUG WIDTH (MM) : 21 <br>
+        BAND TYPE : Bracelet <br>
+        Stainless Steel : CASE MATERIAL <br>
+        Two-Tone <br>
+        Stainless Steel <br>
+        CRYSTAL <br>
+        Sapphire Crystal <br>
+        Superior scratch resistance and hardness <br>
+        CLASP <br>
+        Fold Over Clasp with Push Buttons <br>
+        DIAL <br>
+        Black <br>
+        WATER-RESISTANCE <br>
+        WR100/10Bar/333ft [Swimming, Showering & Snorkeling] <br>
+        ADDITIONAL FUNCTIONS <br>
+        1 Second Chronograph Measures up to 60 Minutes
+        12/24 Hour Time
+        Date`
+    },{
+        id:"cc",
+        name:"Mens Fossil Garrett Chronograph Watch",
+        price:12000,
+        desc:"FS5623",
+        img:"img/c.jpg",
+        popupImages: ["img/c2.jpg", "img/c3.jpg", "img/c4.jpg", "img/c5.jpg"],
+        spec:`Sku: FS5771 <br>
+        Case Size: 44 mm <br>
+        Movement: Chronograph <br>
+        Platform: GARRETT <br>
+        Strap Material: Stainless Steel <br>
+        Strap Colour: Gold, Silver <br>
+        Case Water Resistance: 10 ATM <br>
+        Case Material: Stainless Steel <br>
+        Case Colour: Silver <br>
+        Dial Colour: Black <br>
+        Strap Fashion Colour: Two-Tone: Silver and Gold <br>
+        Interchangeable Compatibility: 20 mm <br>
+        Strap Width: 20 mm <br>
+        Closure: Single Pusher Foldover Clasp <br>
+        Strap Inner Circumference: 200 +/- 5 mm <br>
+        Crystal Type: Mineral <br>
+        Battery Type: SR621SW <br>
+        `
+    },
+    {
+        id:"dd",
+        name:"Vintage Rolex Datejust Silver Dial ",
+        price: 22000,
+        desc:"1603",
+        img:"img/d.jpg",
+        popupImages: ["img/d2.jpg", "img/d3.jpg", "img/d4.jpg", "img/d.jpg"],
+        spec:`Country/Region of Manufacture: Switzerland <br>
+         Style: Classic <br> Model: Rolex Datejust 1603 <br> Vintage: Yes <br> Movement: Mechanical (Automatic) <br> Number of Jewels: 26 Jewels <br> Dial Color: Silver <br> Display: Analog <br> Indices: 12-Hour Dial <br> Stick Indexes <br> Bezel Type: Engine-Turned  Fixed <br> Bezel Color: Steel <br> Band Color: Steel <br>  Closure: Fold-Over Clasp <br> Band Type: Bracelet <br> Band Width: 15.5 mm <br> Lug Width: 20 mm <br> Case Material: Stainless Steel <br> Case Color: Steel <br> Water Resistance: 100 m (10 ATM) <br> Watch Shape: Round <br> Caseback: Screwback <br> Case Finish: Brushed <br> Case Size: 36 mm <br> Case Thickness: 12 mm <br> Features: Date Indicator <br> Handedness: Right <br> Wrist Fit: 6.75 Inches <br> Movement Caliber: Rolex Caliber 1570`
+    },
+    {
+        id:"ee",
+        name:"Tommy Hilfiger The Kane Men's Watch",
+        price:22000,
+        desc:"- 1791400",
+        img:"img/e.jpg",
+        popupImages: ["img/e2.jpg", "img/e3.jpg", "img/e4.jpg", "img/e5.jpg"],
+        spec:`Movement Type: Quartz Multifunction <br>
+        Case Diameter: 44mm <br>
+        Case Thickness: 10.85mm <br>
+        Band/Lug Width: 22mm <br>
+        Case Material: Stainless Steel <br>
+        Crystal Material: Hardened Mineral <br>
+        Dial Index: Roman <br>
+        Dial Colour: Silver & White <br>
+        Strap Material: Brown Leather <br>
+        Water Resistance: 3 ATM <br>
+        Clasp Type: Tongue Buckle <br>
+        ﻿Warranty: 2 Year International Manufacturer's Warranty`
+    },
+    {
+        id:"ff",
+        name:"Tommy Hilfiger Men's Decker Silver Watch",
+        price:6000,
+        desc:"1791564",
+        img:"img/f.jpg",
+        popupImages: ["img/f3.jpg", "img/f2.jpg", "img/f.jpg"],
+        spec:`Brand:Tommy HilfigerModel <br>
+        Number:1791564Case <br>
+         Width:48, mmCase Depth:11mm <br> 
+         Display Type:Chronograph <br> 
+         Material:Stainless <br> 
+         SteelMovement:QuartzStrap <br>
+         Type:Stainless <br> 
+         SteelStrap Width:22 mmDial <br>
+          Colour:BlackStrap <br>
+           Colour:SilverClasp <br>
+           Type:Push-ButtonWater Resistance:50 <br>
+            metersPackaging:Official Tommy Hilfiger Presentation Box
+        `
+    },
+    {
+        id:"gg",
+        name:"Tommy Hilfiger Men's Sophisticated Sport Watch",
+        price:17000,
+        desc:" 1791293",
+        img:"img/g.jpg",
+        popupImages: ["img/g2.jpg", "img/g3.jpg", "img/g4.jpg", "img/g.jpg"],
+        spec:`
+        Movement Type: Quartz Multifunction <br>
+        Case Diameter: 44.98mm <br>
+        Case Thickness: 10mm <br>
+        Band/Lug Width: 22mm <br>
+        Case Material: Stainless Steel <br>
+        Crystal Material: Mineral <br>
+        Dial Index: Arabic Index <br>
+        Dial Colour: Navy <br>
+        Strap Material: Stainless Steel <br>
+        Water Resistance: 5 ATM <br>
+        Clasp Type: Push-Button Deployment`
+    },
+    {
+        id:"ii",
+        name:"Michael kors ladies rose gold darci",
+        price:6500,
+        desc:"MK3369",
+        img:"img/i5.jpg",
+        popupImages: ["img/i2.jpg", "img/i3.jpg", "img/i4.jpg", "img/i.jpg"],
+        spec:`
+        Model-Year : 2018 <br>
+        Dial window material typeMineral <br>
+        Display TypeAnalog <br>
+        Case diameter : 39 millimeters <br>
+        Case Thickness : 7 millimeters <br>
+        Band width : 28 millimeters <br>
+        Band : ColorRose Gold <br>
+        Dial color : Rose GoldMovementQuartz`
+    },
+    {
+        id:"jj",
+        name:"Darci  Gold Stainless-Steel Quartz Fashion Watch",
+        price:7800,
+        desc:"MK3368",
+        img:"img/j.jpg",
+        popupImages: ["img/j2.jpg", "img/j3.jpg", "img/j4.jpg", "img/j.jpg"],
+        spec:`Gold-tone stainless steel case with a gold-tone stainless steel mesh bracelet.
+        Dress watch style. Michael Kors Darci Gold Tone Stainless Steel Ladies Watch MK3368
+        Deployment clasp. Water resistant at 50 meters / 165 feet. 
+         Fixed gold-tone bezel set with crystals. Gold-tone sunray dial with gold-toned hands and index hour markers. <br>
+         Dial Type: Analog <br>
+        Quartz movement : Scratch resistant mineral crystal. Pull / push crown. Solid case back. <br>
+        Case diameter: 39 mm <br>
+        Case thickness: 7.5 mm.<br>
+        Band width: 18 mm. <br>
+        Functions: hour, minute, second. `
+    },
+    {
+        id:"kk",
+        name:"Hugo Boss Pioneer Chronograph Mens",
+        price:11000,
+        desc:"1513712",
+        img:"img/k.jpg",
+        popupImages: ["img/k2.jpg", "img/k3.jpg", "img/k4.jpg", "img/k.jpg"],
+        spec:`Water resistance: 5 ATM <br>
+        Movement: Battery (quartz) <br>
+        Material: Stainless steel <br>
+        Case width: 44 mm`
+    },
+    {
+        id:"ll",
+        name:"Rado Swiss Sapphire Crystal Gold",
+        price:88000 ,
+        desc:"R12413493",
+        img:"img/l2.jpg",
+        popupImages: ["img/l.jpg", "img/l3.jpg", "img/l4.jpg", "img/l5.jpg"],
+        spec:` 
+        Model number :  R12413493 <br>
+        Model Year : 2011 <br>
+        Item Shape : Round <br>
+        Dial window material type : Sapphire Crystal<br>
+        Display Type : Analog<br>
+        Clasp 3 Clasp Closure
+        Case material : Stainless Steel <br>
+        Case diameter : 35 millimeters <br>
+        Case Thickness : 11.9 millimeters <br>
+        Band Material : Stainless Steel <br>
+        Band size Mens : Standard <br>
+        Band width : 21 millimeters<br>
+        Band Color : Gold<br>
+        Dial color : Gold<br>
+        Bezel material : Stainless Steel<br>
+        Calendar : Day-Date<br>
+        Special features : Date Display<br>
+        Item weight : 1.1 Pounds (0.5 kg)<br>
+        Movement : Swiss Automatic<br>
+        Water resistant depth : 30 Meters<br>
+        Product Dimensions : 4.88 x 6.14 x 6.06 inches (12.4 x 15.6 x 15.4 cm); 1.1 Pounds (0.5 kg)<br>
+        Material : Gold-plated-stainless-steel<br>
+        Country of Origin : China<br>`
+    },
+    {
+        id:"mm",
+        name:"Ladies 18K Rolex Diamond Champagne",
+        price:38000,
+        desc:"6917",
+        img:"img/m.jpg",
+        popupImages: ["img/m2.jpg", "img/m3.jpg", "img/m4.jpg", "img/m5.jpg"],
+        spec:`Casing: Original with Serial number (6 o'clock) and Model / Ref. # between lugs (12 o'clock).
+        Movement: 2035/ Calibre mechanical movement <br>
+        Clasp: Authentic Rolex Buckle.Band <br>
+        Bracelet: Original Jubilee 18K <br>
+        Stainless Steel Band.Dial: Aftermarket custom diamond marker <br>
+        dial.Crown: Original Rolex crown.Diamond <br>
+        Bezel: Aftermarket bezel with 100% Genuine Round Diamonds set in-house by experienced diamond setter.`
+    },
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
